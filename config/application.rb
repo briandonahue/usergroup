6b6c6026a4c63bcd865465dcb4228fbe905dc6b7
briandonahue <brian.donahue@vitreosolutions.com>
2011-08-04 22:05:58 -0400

    testing heroku mongo config

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Usergroup
     # Enable the asset pipeline
     config.assets.enabled = true
     if ENV["MONGOHQ_URL"]
-      mongo_uri = URI.parse(ENV["MONGOHQ_URL”])
+      mongo_uri = URI.parse(ENV["MONGOHQ_URL"])
       ENV['MONGOID_HOST'] = mongo_uri.host
       ENV['MONGOID_PORT'] = mongo_uri.port.to_s
       ENV['MONGOID_USERNAME'] = mongo_uri.user
